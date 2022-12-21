21909a025db71eff0e2aaf2f0fcc2065ddc1131d
senseiuc <dhinhojboy@gmail.com>
2022-12-21 19:06:48 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a square class that returns nothing """
+
+
+class Square:
+    """ a square class
+        Attributes: size
+    """
+    def __init__(self, size):
+        self.__size = size

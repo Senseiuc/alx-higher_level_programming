7b409b6e54fd56ec18cf45bbd0f5b0c12dd0cd17
senseiuc <dhinhojboy@gmail.com>
2022-12-21 09:56:13 +0000

    check cycle - incomplete

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+/**
+ *check_cycle - check if a linked list has a cycle
+ *@list: the linked list to be checked
+ *Return: 0 if there's no cycle or 1 if there is.
+ */
+
+int check_cycle(listint_t *list)
+{
+
+}

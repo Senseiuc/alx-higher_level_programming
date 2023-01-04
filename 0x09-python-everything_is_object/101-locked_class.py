ba5fdc1dea2bc3e79b75707b6da5e140daaef1a3
senseiuc <dhinhojboy@gmail.com>
2023-01-04 22:28:27 +0000

    locked_class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,8 +3,4 @@ class LockedClass:
     """ A class that prevents creation of attributes
         except for first name
     """
-    def __setattr__(self,attribute,value):
-        if attribute == 'first_name':
-            super().__setattr__(attribute,value)
-        else:
-            raise AttributeError
+    __slots__ = ['first_name']

3f38b0926f1f08c98f6e43903188036e17a53676
senseiuc <dhinhojboy@gmail.com>
2023-01-11 16:24:46 +0000

    sort list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+   A class that inherits the list object and also sorts a given list
+"""
+
+class MyList(list):
+    """
+        A subclass of list class that sorts the list
+    """
+    def print_sorted(self):
+        """ print sorted list"""
+        print(sorted(self))

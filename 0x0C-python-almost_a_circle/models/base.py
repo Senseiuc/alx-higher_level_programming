c5cf713ae4f4081c5760082e0d0a6bbe73c9c408
senseiuc <dhinhojboy@gmail.com>
2023-01-14 15:33:11 +0000

    base instantiation

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""creating a base class"""
+
+
+class Base:
+    """
+       A class base
+       Attributes:
+           __nb_objects(int): The number of int objects.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+           Initialise the base class
+           Args:
+               id (int): the object id
+        """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

30a1f3996810e5b8279c827ce4bf2f43474f4ac6
senseiuc <dhinhojboy@gmail.com>
2023-01-17 04:39:14 +0000

    remove print yes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,6 +21,23 @@ class Rectangle(Base):
                y (int): position on the y axis
                id (int): the id of the object
         """
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            print('yes')
+            raise ValueError("x must be >= 0")
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.width = width
         self.height = height
         self.x = x

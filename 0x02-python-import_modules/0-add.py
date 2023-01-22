d615d4ad8302e93eefe94ae211c7206f80fbee05
senseiuc <dhinhojboy@gmail.com>
2023-01-22 19:13:24 +0000

    import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """a function that imports add function"""
+from add_0 import add
 
 if __name__ == '__main__':
-    from add_0 import add
     a = 1
     b = 2
     print(f'{a} + {b} = {add(a, b)}')

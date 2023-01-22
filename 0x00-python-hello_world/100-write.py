d913e51841ed831a4af70ddd80b3098d50773564
senseiuc <dhinhojboy@gmail.com>
2023-01-22 09:41:55 +0000

    write to stderr

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A python code that prints using write from the
+   system module
+"""
+import sys
+
+sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
+sys.exit(1)

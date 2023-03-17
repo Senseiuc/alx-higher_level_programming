c491a15ff74c86026feb6691ec1e7e0d60036b03
senseiuc <dhinhojboy@gmail.com>
2023-03-17 06:41:56 +0000

    filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A script that lists all states
+from the database hbtn_0e_0_usa
+starting with N
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    arg = sys.argv
+    db = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
+                         passwd=arg[2], db=arg[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+                ORDER BY states.id ASC".format(arg[4]))
+    data = cur.fetchall()
+    for i in data:
+        print(i)
+    cur.close()
+    db.close()

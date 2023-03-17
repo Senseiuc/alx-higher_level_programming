aae4438ef22b1e6c622dcbfd44358361cbff42f6
senseiuc <dhinhojboy@gmail.com>
2023-03-17 10:08:17 +0000

    states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 """
 declare a base
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Identity
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -10,6 +10,5 @@ Base = declarative_base()
 
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False,
-                unique=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)

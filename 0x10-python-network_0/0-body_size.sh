7bf5f340b3c7e4f4cdbb7ff6dc670074e4adea75
senseiuc <dhinhojboy@gmail.com>
2023-03-30 18:46:35 +0000

    display body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -3,4 +3,4 @@
 # ,sends a request to that URL, 
 #and displays the size of the body of the response
 
-curl -Is "$1" | grep Content-Length | cut -d ' ' -f 2
+curl -Is "$1" | grep "Content-Length" | cut -d ' ' -f 2

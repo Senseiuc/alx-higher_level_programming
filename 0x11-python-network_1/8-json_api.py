42c12a772906c39b12a939ed3110fd8bd09538fc
senseiuc <dhinhojboy@gmail.com>
2023-03-31 18:19:10 +0000

    json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a script that takes in a URL and an email,
+sends a POST request to the passed URL
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ''
+    value = {'q': q}
+    r = requests.post(url, value).json()
+    try:
+        if 'id' in r and 'name' in r:
+            print("[{}] {}".format(r['id'], r['name']))
+        else:
+            print('No result')
+    except:
+        print('Not a valid JSON')
+

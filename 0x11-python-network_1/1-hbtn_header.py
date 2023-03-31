5c6c2ac0242ac46234a76e3e50dd4b5b4fb5d9aa
senseiuc <dhinhojboy@gmail.com>
2023-03-31 16:23:21 +0000

    X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,5 +7,4 @@ import urllib.request
 import sys
 
 with urllib.request.urlopen(sys.argv[1]) as response:
-    html = response.info()
-print(html.get("X-Request-Id"))
+    print(response.info().get("X-Request-Id"))

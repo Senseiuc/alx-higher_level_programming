52353fb9817c4f5a7a183a8dc46bb0eb5f9e46fc
senseiuc <dhinhojboy@gmail.com>
2023-04-25 19:31:16 +0000

    fixed starware

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
  * a script that prints starwars title
  */
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });

7cfba8b219b349eb1ac617c94d25bd2b4c3d6167
senseiuc <dhinhojboy@gmail.com>
2023-04-25 20:48:33 +0000

    in order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,25 @@
 #!/usr/bin/node
-/*
- * a script that prints starwars actors
- */
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    for (const i of JSON.parse(body).characters) {
-      request(i, function (error, response, body) {
-        console.log(error || JSON.parse(body).name);
-      });
-    }
+const endPoint = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(endPoint, function (err, response, body) {
+  if (err) {
+    throw err;
+  } else if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    const myList = [];
+    characters.forEach(character => {
+      myList.push(new Promise((resolve, reject) => {
+        request.get(character, function (err, response, body) {
+          if (err) {
+            reject(err);
+          } else if (response.statusCode === 200) {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      }));
+    });
+    Promise.all(myList).then(names => {
+      names.forEach(name => console.log(name));
+    });
   }
 });
